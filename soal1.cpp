e635496f3e8f3bfdde8a85edc5df2643944098cd
shi44yon <33504580+shi44yon@users.noreply.github.com>
2017-11-19 22:05:50 +0700

    Create soal1.cpp

---
+++ soal1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int bil[10];
+	for(int i = 0; i < 10; i ++) {
+		cin>>bil[i];
+	}
+	cout<<"Terkecil: "<<cekKecil<<endl;
+	cout<<"Terbesar: "<<cekBesar<<endl;
+}
+
+int cekKecil(int bil[i]) {
+	int kecil = bil[0];
+	for(int i = 0; i < 10; i++){
+		if(bil[i] < kecil){
+			kecil = bil[i];
+		}
+	}
+	return kecil;
+}
+
+int cekBesar(int bil[i]) {
+	int besar = bil[0];
+	for(int i = 0; i < 10; i++){
+		if(bil[i] > besar){
+			besar = bil[i];
+		}
+	}
+	return besar;
+}
